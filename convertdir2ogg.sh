c66c8fbfce0922986ece8045b01469d7125fe992
Michael B. Kulik <mike.b.kulik@gmail.com>
2011-01-06 11:47:48 -0500

    Fixed issuse with output ffmpeg2theora program

--- convertdir2ogg.sh
+++ convertdir2ogg.sh
@@ -4,17 +4,17 @@ for i in $( ls $1 );
 do
     if [[ $i == *".mp4"* ]]; then
 
-        echo "Converting $i to ogv\n"
+        echo "Converting $i to ogv"
 
         ogvfile="${1}/${i/.mp4/.ogv}"
         #echo "ogvfile is: $ogvfile\n"
 
         if [ -f $ogvfile ];
         then
-            echo "ogv file already exists, skipping conversion"
+            echo "$ogvfile already exists, skipping conversion"
         else
-            ffmpeg2theora $1
-            echo "\nFinished Converting!\n";
+            ffmpeg2theora $1 --output $ogvfile
+            echo "Finished Converting!\n";
         fi
     fi
 done
