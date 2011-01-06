f12c7bab0af01a89169d0922927ae8543a2cedaf
Michael B. Kulik <mike.b.kulik@gmail.com>
2011-01-06 11:42:22 -0500

    Need read man page on ffmepg2theora to deptermine if you can name the output file, so it can work in the script without cd to movies directory

--- convertdir2ogg.sh
+++ convertdir2ogg.sh
@@ -2,7 +2,19 @@
 
 for i in $( ls $1 );
 do
-echo "Converting $i to ogv\n"
-ffmpeg2theora $1
-echo "\nFinished Converting!\n";
+    if [[ $i == *".mp4"* ]]; then
+
+        echo "Converting $i to ogv\n"
+
+        ogvfile="${1}/${i/.mp4/.ogv}"
+        #echo "ogvfile is: $ogvfile\n"
+
+        if [ -f $ogvfile ];
+        then
+            echo "ogv file already exists, skipping conversion"
+        else
+            ffmpeg2theora $1
+            echo "\nFinished Converting!\n";
+        fi
+    fi
 done
