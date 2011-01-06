22b1c2436775a4b2a1109e28c716ca23a291150d
Michael B. Kulik <mike.b.kulik@gmail.com>
2011-01-06 11:17:11 -0500

    Fixed ogg file extension to ogv

--- video.php
+++ video.php
@@ -11,7 +11,7 @@ if( $video != '' )
     echo "\t<source src=\"movies/" . $video 
          . ".mp4\" type=\"video/mp4\" />\n";
     echo "\t<source src=\"movies/" . $video
-         . ".ogg\" type=\"video/ogg\" />\n";
+         . ".ogv\" type=\"video/ogg\" />\n";
     echo "Your Browser does not support video streaming html5 tags\n";
     echo "</video>";
 }
