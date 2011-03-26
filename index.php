8f695ddd0c05711ec6eb3e51e60da55fe0fa1977
Michael B. Kulik <mike.b.kulik@gmail.com>
2011-03-26 17:21:09 -0400

    Added a way to visually see what movies also had a webm file and supported on all browsers

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <head></head>
 <body>
 
+<br /> A file marked with a "*" is supported on all browsers<br /><br />
+
 <?php
 $dir = "movies";
 
@@ -14,10 +16,23 @@ foreach( $files as $file )
     {
         //replace .mp4 with nothing
         $stripped_name = str_replace(".mp4", "", $file);
-      
+        
+        //check to see if a .webm file exists, then movie
+        // plays on all browsers
+        $full_support = '';
+
+        $webm_file = './' . $dir . '/' . $stripped_name . '.webm';
+        //echo $webm_file;
+
+        if( file_exists($webm_file) == TRUE )
+        {
+            //echo "Webm file exists";
+            $full_support = '*';
+        }
+
         //set up the link
         echo "<a href=\"video.php?vid=" . $stripped_name ."\">" .
-        $stripped_name . "</a><br />";
+        $stripped_name . "</a> " . $full_support . "<br />";
     }
 }
 ?>
