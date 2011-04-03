3da0dc379267e4560368a9b6adff7b15886ee5e9
Michael B. Kulik <mike.b.kulik@gmail.com>
2011-04-02 21:32:28 -0400

    Forgot some files

--- deploy_mac.sh
+++ deploy_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp index.php /Library/WebServer/Documents/
-cp video.php /Library/WebServer/Documents/
+cp *.html /Library/WebServer/Documents/
+cp *.php /Library/WebServer/Documents/
--- video.php
+++ video.php
@@ -3,14 +3,27 @@
 
 <?php 
 $video = $_GET['vid'];
+$dir = "movies/";
 
 if( $video != '' )
 {
     echo "<video controls=\"controls\">\n";
-    echo "\t<source src=\"movies/" . $video 
-         . ".mp4\" type=\"video/mp4\" />\n";
-    echo "\t<source src=\"movies/" . $video
-         . ".webm\" type=\"video/webm\" />\n";
+    echo "\t<source src=\"" . $dir . $video . "\"";
+
+    if( strpos($video, ".mp4") == TRUE )
+    {
+        echo " type=\"video/mp4\" />\n";
+    }
+    else
+    {
+        echo " type=\"video/webm\" />\n";
+    }
+
+    
+    
+    #. ".mp4\" type=\"video/mp4\" />\n";
+    #echo "\t<source src=\"movies/" . $video
+    #     . ".webm\" type=\"video/webm\" />\n";
     echo "Your Browser does not support video streaming html5 tags\n";
     echo "</video>";
 }
